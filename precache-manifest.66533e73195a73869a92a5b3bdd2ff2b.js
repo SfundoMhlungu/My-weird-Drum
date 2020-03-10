8da273264036418ab559b3c1ed903e97d54146bb
Sfundo Mhlungu <mhlungusk@gmail.com>
2020-03-10 21:30:33 +0200

    MyDrum don't judge, i didn't find sounds

---
+++ precache-manifest.66533e73195a73869a92a5b3bdd2ff2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11948f1ca8054048a75fc0654ff6e9eb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "963b7379aa9a300f8f43",
+    "url": "/static/css/main.725b2dad.chunk.css"
+  },
+  {
+    "revision": "7c43f04557d8947b5aac",
+    "url": "/static/js/2.8c28f7c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.8c28f7c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "963b7379aa9a300f8f43",
+    "url": "/static/js/main.d3663205.chunk.js"
+  },
+  {
+    "revision": "2c8039270614a39c8cea",
+    "url": "/static/js/runtime-main.44716643.js"
+  }
+]);
